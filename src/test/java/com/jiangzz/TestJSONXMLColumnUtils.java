fd2b56ccb2b63beb27891649868827b1b3584d2d
jiangzz <2237257164@qq.com>
2021-03-07 15:57:53 +0800

    Update TestJSONXMLColumnUtils.java
    适配了JSON Schema解析

--- src/test/java/com/jiangzz/TestJSONXMLColumnUtils.java
+++ src/test/java/com/jiangzz/TestJSONXMLColumnUtils.java
@@ -6,9 +6,8 @@ import org.junit.Test;
 import java.util.List;
 
 public class TestJSONXMLColumnUtils {
-    String xmlContent="<?xml version=\"1.0\" encoding=\"UTF-8\" ?><message><id>1</id><name>zhangsan</name><sex>true</sex><store><books><book><category>reference</category><author>Nigel Rees</author><title>Sayings of the Century</title><price>8.95</price></book><book><category>reference</category><author>Nigel Rees</author><title>Sayings of the Century</title><price>9.95</price></book></books><bicycles><color>yellow</color><price>19.95</price></bicycles></store></message>";
-    String jsonContent="{\"id\":1,\"name\":\"jiangzz\",\"address\":{\"city\":\"beijing\",\"street\":\"东北旺西路\"},\"salary\":20000.8,\"sex\":true,\"friends\":[{\"id\":1,\"name\":\"张三\"},{\"id\":1,\"name\":\"张三\"}]}";
-
+    String xmlContent="<message><id>1</id><name>zhangsan</name><sex>true</sex><store><books><book><category>reference</category><author>Nigel Rees</author><title>Sayings of the Century</title><price>8.95</price></book><book><category>reference</category><author>Nigel Rees</author><title>Sayings of the Century</title><price>9.95</price></book></books><bicycles><color>yellow</color><price>19.95</price></bicycles></store></message>";
+    String jsonContent="{\"video\":{\"id\":\"29BA6ACE7A9427489C33DC5901307461\",\"title\":\"体验课01\",\"desp\":\"描述信息\",\"tags\":[\"用户\",\"旅游\"],\"cards\":[1,2,3,10.7],\"pet\":{\"nmae\":\"花花\",\"hobbies\":[\"TV\",\"GAME\"]},\"duration\":503,\"category\":\"07AD1E11DBE6FDFC\",\"image\":\"0.jpg\",\"imageindex\":0,\"SmallImages\":[{\"index\":0,\"url\":\"0.jpg\",\"other\":[{\"id\":11,\"name\":\"xxx\"}]}]}}";
     @Test
     public void testXMLPATHSchema() throws Exception {
         System.out.println(xmlContent);
@@ -21,147 +20,12 @@ public class TestJSONXMLColumnUtils {
         List<JSONXMLSchemaUtils.MateNode> columnInfos = JSONXMLSchemaUtils.generateXMLTreeSchema(xmlContent,false);
         System.out.println(new ObjectMapper().writeValueAsString(columnInfos));
     }
-    /**
-     * {
-     *     "id": 1,
-     *     "name": "jiangzz",
-     *     "address": {
-     *         "city": "beijing",
-     *         "street": "东北旺西路"
-     *     },
-     *     "salary": 20000.8,
-     *     "sex": true,
-     *     "friends": [
-     *         {
-     *             "id": 1,
-     *             "name": "张三"
-     *         },
-     *         {
-     *             "id": 1,
-     *             "name": "张三"
-     *         }
-     *     ]
-     * }
-     *
-     * 返回
-     * [
-     *     {
-     *         "columnPath": "address",
-     *         "columnType": "object"
-     *     },
-     *     {
-     *         "columnPath": "address.city",
-     *         "columnType": "string"
-     *     },
-     *     {
-     *         "columnPath": "address.street",
-     *         "columnType": "string"
-     *     },
-     *     {
-     *         "columnPath": "friends",
-     *         "columnType": "array"
-     *     },
-     *     {
-     *         "columnPath": "friends.id",
-     *         "columnType": "int"
-     *     },
-     *     {
-     *         "columnPath": "friends.name",
-     *         "columnType": "string"
-     *     },
-     *     {
-     *         "columnPath": "id",
-     *         "columnType": "int"
-     *     },
-     *     {
-     *         "columnPath": "name",
-     *         "columnType": "string"
-     *     },
-     *     {
-     *         "columnPath": "salary",
-     *         "columnType": "float"
-     *     },
-     *     {
-     *         "columnPath": "sex",
-     *         "columnType": "boolean"
-     *     }
-     * ]
-     * @throws Exception
-     */
     @Test
     public void testJSONPATHSchema() throws Exception {
-        System.out.println(jsonContent);
         List<JSONXMLSchemaUtils.ColumnInfo>   columnInfos= JSONXMLSchemaUtils.generateJSONPathSchema(jsonContent);
         System.out.println(new ObjectMapper().writeValueAsString(columnInfos));
     }
 
-    /**
-     * [
-     *     {
-     *         "name": "address",
-     *         "fullPath": "address",
-     *         "type": "object",
-     *         "childNodes": [
-     *             {
-     *                 "name": "city",
-     *                 "fullPath": "address.city",
-     *                 "type": "string",
-     *                 "childNodes": null
-     *             },
-     *             {
-     *                 "name": "street",
-     *                 "fullPath": "address.street",
-     *                 "type": "string",
-     *                 "childNodes": null
-     *             }
-     *         ]
-     *     },
-     *     {
-     *         "name": "friends",
-     *         "fullPath": "friends",
-     *         "type": "array",
-     *         "childNodes": [
-     *             {
-     *                 "name": "id",
-     *                 "fullPath": "friends.id",
-     *                 "type": "int",
-     *                 "childNodes": null
-     *             },
-     *             {
-     *                 "name": "name",
-     *                 "fullPath": "friends.name",
-     *                 "type": "string",
-     *                 "childNodes": null
-     *             }
-     *         ]
-     *     },
-     *     {
-     *         "name": "id",
-     *         "fullPath": "id",
-     *         "type": "int",
-     *         "childNodes": null
-     *     },
-     *     {
-     *         "name": "name",
-     *         "fullPath": "name",
-     *         "type": "string",
-     *         "childNodes": null
-     *     },
-     *     {
-     *         "name": "salary",
-     *         "fullPath": "salary",
-     *         "type": "float",
-     *         "childNodes": null
-     *     },
-     *     {
-     *         "name": "sex",
-     *         "fullPath": "sex",
-     *         "type": "boolean",
-     *         "childNodes": null
-     *     }
-     * ]
-     * @throws Exception
-     */
     @Test
     public void testJSONTreeSchema() throws Exception {
         System.out.println(jsonContent);
